22e2d61a2e240a84ccd5eb7bc2879e87adc2f75a
peterejiro <peterejiro96@gmail.com>
2021-05-18 10:24:50 +0100

    side menu

--- resources/views/livewire/backend/partials/sidebar-menu.blade.php
+++ resources/views/livewire/backend/partials/sidebar-menu.blade.php
@@ -87,78 +87,69 @@
 											<span class="pcoded-mtext">HR</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('hr dashboard')
+
 
 										<li class=" ">
 												<a href="{{route('hr-dashboard')}}">
 														<span class="pcoded-mtext">HR Dashboard</span>
 												</a>
 										</li>
-										@endcan
-										@can('view employees')
+
 											<li class=" ">
 													<a href="{{route('employees')}}">
 															<span class="pcoded-mtext">Employees</span>
 													</a>
 											</li>
 
-										@endcan
-										@can('view appreciation')
+
 
 										<li class=" ">
 												<a href="{{ route('appreciation') }}">
 														<span class="pcoded-mtext">Appreciation</span>
 												</a>
 										</li>
-										@endcan
-										@can('view resignation')
 
 										<li class=" ">
 												<a href="{{ route('resignation') }}">
 														<span class="pcoded-mtext">Resignation</span>
 												</a>
 										</li>
-										@endcan
-										@can('view termination')
+
 
 										<li class=" ">
 												<a href="{{ route('terminated-employment') }}">
 														<span class="pcoded-mtext">Termination</span>
 												</a>
 										</li>
-										@endcan
-										@can('view ideabox')
+
 
 										<li class=" ">
 												<a href="{{route('hr-ideabox')}}">
 														<span class="pcoded-mtext">IdeaBox</span>
 												</a>
 										</li>
-										@endcan
-										@can('view queries')
+
 
 										<li class=" ">
 												<a href="{{route('queries')}}">
 														<span class="pcoded-mtext">Queries</span>
 												</a>
 										</li>
-										@endcan
-										@can('onboard employee')
+
 
 										<li class=" ">
 												<a href="{{route('on-boarding')}}">
 														<span class="pcoded-mtext">onBoarding</span>
 												</a>
 										</li>
-										@endcan
-										@can('view hr config')
+
 
 										<li class=" ">
 												<a href="{{route('hr-configurations')}}">
 														<span class="pcoded-mtext">HR Configurations</span>
 												</a>
 										</li>
-										@endcan
+
 
 									</ul>
 							</li>
@@ -168,30 +159,28 @@
 											<span class="pcoded-mtext">Timesheet</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('view attendance')
+
 
 										<li class=" ">
 												<a href="{{route('attendance')}}">
 														<span class="pcoded-mtext">Attendance</span>
 												</a>
 										</li>
-										@endcan
-										@can('manage leave')
+
 
 										<li class=" ">
 												<a href="{{ route('leave-management') }}">
 														<span class="pcoded-mtext">Leave Management</span>
 												</a>
 										</li>
-										@endcan
-										@can('set leave type')
+
 
 										<li class=" ">
 												<a href="{{ route('leave-type') }}">
 														<span class="pcoded-mtext">Leave Types</span>
 												</a>
 										</li>
-										@endcan
+
 									</ul>
 							</li>
 							<li class="pcoded-hasmenu">
@@ -200,22 +189,20 @@
 											<span class="pcoded-mtext">Performance</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('set performance indicator')
+
 
 										<li class=" ">
 												<a href="{{route('performance-indicator')}}">
 														<span class="pcoded-mtext">Performance Indicator</span>
 												</a>
 										</li>
-										@endcan
-										@can('employee appraisal')
 
 										<li class=" ">
 												<a href="{{route('employees-appraisal')}}">
 														<span class="pcoded-mtext">Employee Appraisal</span>
 												</a>
 										</li>
-										@endcan
+
 									</ul>
 							</li>
 					</ul>
@@ -228,14 +215,14 @@
 											<span class="pcoded-mtext">CRM</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('crm dashboard')
+
 											<li class=" ">
 													<a href="{{route('crm-dashboard')}}">
 															<span class="pcoded-mtext">Dashboard</span>
 													</a>
 											</li>
 
-										@endcan
+
 											<li class=" ">
 													<a href="{{route('clients')}}">
 															<span class="pcoded-mtext">Clients</span>
@@ -275,7 +262,7 @@
 							</li>
 					</ul>
 
-				@can('accounting')
+
 					<div class="pcoded-navigatio-lavel">Accounting</div>
 					<ul class="pcoded-item pcoded-left-item">
 							<li class="">
@@ -286,37 +273,34 @@
 					</li>
 				</ul>
         <ul class="pcoded-item pcoded-left-item">
-            <liclass="pcoded-hasmenu">
+            <li class="pcoded-hasmenu">
 									<a href="javascript:void(0)">
 											<span class="pcoded-micon"><i class="icofont icofont-money-bag"></i></span>
 											<span class="pcoded-mtext">Setup</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('view chart of accounts')
+
 
 										<li class=" ">
 												<a href="{{route('chart-of-accounts')}}">
 														<span class="pcoded-mtext">Chart of Accounts</span>
 												</a>
 										</li>
-										@endcan
-										@can('set budget profile')
+
 
 										<li class=" ">
 												<a href="{{route('budget-profile')}}">
 														<span class="pcoded-mtext">Budget Profile</span>
 												</a>
 										</li>
-										@endcan
-										@can('set budget')
+
 
 										<li class=" ">
 												<a href="{{route('budget-setup')}}">
 														<span class="pcoded-mtext">Budget Setup</span>
 												</a>
 										</li>
-										@endcan
-										@can('set opening balance')
+
 
 											<li class=" ">
 													<a href="{{route('bank-accounts')}}">
@@ -329,11 +313,11 @@
                         </a>
                     </li>
                     <li class=" ">
-                        <ahref="{{route('opening-balance')}}">
+                        <a href="{{route('opening-balance')}}">
 															<span class="pcoded-mtext">Opening Balance</span>
 													</a>
 											</li>
-										@endcan
+
 											<li class=" ">
 													<a href="{{route('accounting-vat')}}">
 															<span class="pcoded-mtext">VAT</span>
@@ -373,8 +357,7 @@
 							</li>
 					</ul>
 
-				@endcan
-				@can('access vendors')
+
         <ul class="pcoded-item pcoded-left-item">
             <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
@@ -382,20 +365,19 @@
                     <span class="pcoded-mtext">Vendors</span>
                 </a>
                 <ul class="pcoded-submenu">
-									@can('view vendor bills')
+
 
 									<li class=" ">
 											<a href="{{route('vendor-bills')}}">
 													<span class="pcoded-mtext">Vendor Bills</span>
 											</a>
 									</li>
-									@endcan
+
                 </ul>
             </li>
         </ul>
 
-				@endcan
-				@can('access customers')
+
         <ul class="pcoded-item pcoded-left-item">
             <li class="pcoded-hasmenu">
                 <a href="javascript:void(0)">
@@ -417,8 +399,7 @@
             </li>
         </ul>
 
-				@endcan
-				@can('post transactions')
+
 					<ul class="pcoded-item pcoded-left-item">
 							<li class="pcoded-hasmenu">
 									<a href="javascript:void(0)">
@@ -426,7 +407,7 @@
 											<span class="pcoded-mtext">Postings</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('post transactions')
+
 
 											<li class=" ">
 													<a href="{{route('receipt-posting')}}">
@@ -448,13 +429,12 @@
 															<span class="pcoded-mtext">Workflow</span>
 													</a>
 											</li>
-										@endcan
+
 									</ul>
 							</li>
 					</ul>
 
-				@endcan
-				@can('generate report')
+
 					<ul class="pcoded-item pcoded-left-item">
 							<li class="pcoded-hasmenu">
 									<a href="javascript:void(0)">
@@ -462,36 +442,33 @@
 											<span class="pcoded-mtext">Reports</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('acess trial balance')
+
 
 											<li class=" ">
 													<a href="{{route('trial-balance')}}">
 															<span class="pcoded-mtext">Trial Balance</span>
 													</a>
 											</li>
-										@endcan
-										@can('access balance sheet')
+
 											<li class=" ">
 													<a href="{{route('balance-sheet')}}">
 															<span class="pcoded-mtext">Balance Sheet</span>
 													</a>
 											</li>
 
-										@endcan
-										@can('access profit or loss')
+
 
 										<li class=" ">
 												<a href="{{route('profit-o-loss')}}">
 														<span class="pcoded-mtext">Profit/Loss</span>
 												</a>
 										</li>
-										@endcan
+
 									</ul>
 							</li>
 					</ul>
 
-				@endcan
-				@can('procurement')
+
 					<div class="pcoded-navigatio-lavel">Procurement</div>
 					<ul class="pcoded-item pcoded-left-item">
 							<li class="pcoded-hasmenu">
@@ -500,44 +477,41 @@
 											<span class="pcoded-mtext">Procurement</span>
 									</a>
 									<ul class="pcoded-submenu">
-										@can('add vendor')
+
 
 										<li class=" ">
 												<a href="{{route('new-supplier')}}">
 														<span class="pcoded-mtext">Add New Vendor</span>
 												</a>
 										</li>
-										@endcan
-											@can('view vendors')
+
 												<li class=" ">
 														<a href="{{route('suppliers')}}">
 																<span class="pcoded-mtext">Vendors</span>
 														</a>
 												</li>
 
-											@endcan
-											@can('view purchase orders')
+
 
 											<li class=" ">
 													<a href="{{route('purchase-orders')}}">
 															<span class="pcoded-mtext">Purchase Orders</span>
 													</a>
 											</li>
-											@endcan
-											@can('view services')
+
 
 											<li class=" ">
 													<a href="{{route('vendor-services')}}">
 															<span class="pcoded-mtext">Services</span>
 													</a>
 											</li>
-											@endcan
+
 									</ul>
 							</li>
 					</ul>
 
-				@endcan
-				@can('access logistics')
+
+
 					<div class="pcoded-navigatio-lavel">Fleet Management</div>
 					<ul class="pcoded-item pcoded-left-item">
 							<li class="pcoded-hasmenu">
@@ -550,21 +524,20 @@
 									<span class="pcoded-mtext">Renewal Type</span>
 								</a>
 							</li>
-										@can('view drivers')
 
-										<li class=" ">
-												<a href="{{route('logistics-customers')}}">
-									<span class="pcoded-mtext">Maintenance Activity</span>
-								</a>
-							</li>
+
+{{--										<li class=" ">--}}
+{{--												<a href="{{route('logistics-customers')}}">--}}
+{{--									<span class="pcoded-mtext">Maintenance Activity</span>--}}
+{{--								</a>--}}
+{{--							</li>--}}
 
 							<!--<li class=" ">
 									<a href="'logistics-shipping')}}">
 														<span class="pcoded-mtext">Shipping</span>
 												</a>
 										</li>
-										@endcan
-										@can('view customers')
+
 											<li class=" ">
 													<a href="'logistics-pick-up-points')}}">
 											<span class="pcoded-mtext">Pick-up Points</span>
@@ -584,8 +557,7 @@
 													</a>
 											</li>
 
-										@endcan
-										@can('view vehicles')
+
 
 										<li class=" ">
 												<a href="{{route('logistics-vehicles')}}">
@@ -596,21 +568,19 @@
                             <span class="pcoded-mtext">Renewal Schedule</span>
                         </a>
                     </li>
-										@endcan
-										@can('view logistics log')
+
 											<li class=" ">
 													<a href="{{route('all-logs')}}">
 															<span class="pcoded-mtext">Log</span>
 													</a>
 											</li>
 
-										@endcan
+
 									</ul>
 							</li>
 					</ul>
 
-				@endcan
-				@can('update general settings')
+
 					<div class="pcoded-navigatio-lavel">System Settings</div>
 					<ul class="pcoded-item pcoded-left-item">
 							<li class="pcoded-hasmenu">
@@ -628,7 +598,6 @@
 							</li>
 					</ul>
 
-				@endcan
 				@can('access administrative area')
 					<div class="pcoded-navigatio-lavel">Administration</div>
 					<ul class="pcoded-item pcoded-left-item">
